42264de2e913287e4588ea4ee80c6babd287007c
someone <someone@someplace.com>
2020-03-16 20:34:50 +0800

    some init msg

---
+++ Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入要进行运算的两个整数：");   
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            calcuate ca = new calcuate();
+            if (a % 1 != 0 || b % 1 != 0)
+            {
+                Console.WriteLine("输入错误！ 请重新输入");
+                int i = Convert.ToInt32(Console.ReadLine());
+                int j = Convert.ToInt32(Console.ReadLine());
+                a = i;
+                b = j;
+            }
+            else
+            if (a % 1 == 0 & b % 1 == 0)
+            {
+                Console.WriteLine("输入正确，请继续选择相应的操作");
+                Console.WriteLine("*********选择加法请按1********");
+                Console.WriteLine("*********选择减法请按2********");
+                Console.WriteLine("*********选择乘法请按3********");
+                Console.WriteLine("*********选择除法请按4********");
+                int m = Convert.ToInt32(Console.ReadLine());
+                if (m == 1)
+                {
+                    Console.WriteLine(ca.plus(a, b));
+                }
+                //ca.Equals
+                else
+                    if (m == 2)
+                {
+                    
+                    Console.WriteLine(ca.minus(a, b));
+                }
+                else
+                    if (m == 3)
+                {
+                    Console.WriteLine(ca.multiplication(a, b));
+                }
+                
+                else
+                    if (m == 4)
+                {
+                    Console.WriteLine(ca.division(a, b));
+                }
+                else
+                    Console.WriteLine("输入错误！");
+            }
+            ca.Equals(a, b);    
+
+            
+            
+        }
+    }
+}
---
+++ Calculator/calcuate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class calcuate
+    {
+        
+        public int plus(int a,int b)
+        {
+            return a + b;
+        }
+        public int minus(int a, int b)
+        {
+            return a - b;
+        }
+        public int multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public float division(int a, int b)
+        {
+            //float c;
+            //(float)number
+            return (float)a / b;
+        }
+        public void Equals(int a,int b)
+        {
+            if (a == b)
+                Console.WriteLine("这两个整数相等");
+            else
+                Console.WriteLine("这两个整数不相等");
+        }
+    }
+}
